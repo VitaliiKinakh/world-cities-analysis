6832f7ac66b6af757f560776c025ef8e3b93a397
Vitaliy Kinakh <kinakh.vitalii@gmail.com>
2017-12-27 19:47:39 +0200

    Changes: added analysis for continents, and for different cities based on population

--- main.py
+++ main.py
@@ -6,18 +6,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
+continents = ["Europe", "Asia", "Africa", "Australia", "North_America", "South_America"]
+
+population_ranges = [100000, 500000, 1000000]
+
+def get_country_codes():
+    europe_iso_countries = pd.read_html("https://www.countrycallingcodes.com/iso-country-codes/europe-codes.php")[1][1]
+    europe_iso_countries.dropna(inplace=True)
+    europe_iso_countries = europe_iso_countries[europe_iso_countries.map(lambda x: len(str(x)) == 2)]
+    asia_iso_countries = pd.read_html("https://www.countrycallingcodes.com/iso-country-codes/asia-codes.php")[1][1]
+    asia_iso_countries.dropna(inplace=True)
+    asia_iso_countries = asia_iso_countries[asia_iso_countries.map(lambda x: len(str(x)) == 2)]
+    africa_iso_countries = pd.read_html("https://www.countrycallingcodes.com/iso-country-codes/africa-codes.php")[1][1]
+    africa_iso_countries.dropna(inplace=True)
+    africa_iso_countries = africa_iso_countries[africa_iso_countries.map(lambda x: len(str(x)) == 2)]
+    australia_iso_countries = pd.read_html("https://www.countrycallingcodes.com/iso-country-codes/australia-codes.php")[1][1]
+    australia_iso_countries.dropna(inplace=True)
+    australia_iso_countries = australia_iso_countries[australia_iso_countries.map(lambda x: len(str(x)) == 2)]
+    north_america_iso_countries = pd.read_html("https://www.countrycallingcodes.com/iso-country-codes/north-america-codes.php")[1][1]
+    north_america_iso_countries.dropna(inplace=True)
+    north_america_iso_countries = north_america_iso_countries[north_america_iso_countries.map(lambda x: len(str(x)) == 2)]
+    south_america_iso_countries = pd.read_html("https://www.countrycallingcodes.com/iso-country-codes/south-america-codes.php")[1][1]
+    south_america_iso_countries.dropna(inplace=True)
+    south_america_iso_countries = south_america_iso_countries[south_america_iso_countries.map(lambda x: len(str(x)) == 2)]
+
+    iso_codes = pd.DataFrame()
+    iso_codes["Europe"] = europe_iso_countries
+    iso_codes["Asia"] = asia_iso_countries
+    iso_codes["Africa"] = africa_iso_countries
+    iso_codes["Australia"] = australia_iso_countries
+    iso_codes["North_America"] = north_america_iso_countries
+    iso_codes["South_America"] = south_america_iso_countries
+    return iso_codes
+
 
 def basic_statistic_values(df):
     min = df.min()
     max = df.max()
     mean = df.mean()
     median = df.median()
-    mode = df.mode()
     std = df.std()
     return {"min" : min, "max" : max, "mean" : mean, "median" : median,  "std" : std}
 
 
 def create_plot(df, col, name, n_bins, y_tic, x_tic):
+    title = col.lower().capitalize()
     first_df = df[df[col] != 0][col]
 
     fig = plt.figure()
@@ -37,7 +70,7 @@ def create_plot(df, col, name, n_bins, y_tic, x_tic):
     ax1.set_xticks(bins1)
     ax1.set_xticklabels(bins1, rotation=85)
     ax1.xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-    ax1.set_title(name)
+    ax1.set_title(title)
     vals = ax1.get_yticks()
     ax1.set_yticklabels(['{}'.format(int(x * 4)) for x in vals])
 
@@ -63,54 +96,99 @@ def create_plot(df, col, name, n_bins, y_tic, x_tic):
     ax2.set_yticklabels(['{}'.format(int(x * 0.4)) for x in vals])
 
     fig.tight_layout()
-    plt.savefig(str(name) + ".png", dpi=300)
+    plt.savefig(name + "_" + title + ".png", dpi=300)
+
 
 if __name__ == "__main__":
-    cities_db = gpd.read_file("cities.shp")
-    cities_db_with_population = pd.read_csv("worldcitiespop.txt", encoding = "ISO-8859-1")
+    # Variable for storing results
+    results = {}
 
-    cities_db = cities_db.to_crs({'init': 'epsg:3857'})
+    world_cities_db_with_area = gpd.read_file("cities.shp")
+    world_cities_db_with_population = pd.read_csv("worldcitiespop.txt", encoding = "ISO-8859-1")
 
     # Get cities area
-    cities_db["AREA"] = cities_db["geometry"].area/ 10**6 # in km
+    world_cities_db_with_area = world_cities_db_with_area.to_crs({'init': 'epsg:3857'})
+    world_cities_db_with_area["AREA"] = world_cities_db_with_area["geometry"].area/ 10**6 # in km
+
     # Get names in uppercase
-    cities_db_with_population["NAME"] = cities_db_with_population["AccentCity"].map(lambda x: x.upper())
+    world_cities_db_with_population["NAME"] = world_cities_db_with_population["AccentCity"].map(lambda x: x.upper())
+    world_cities_db_with_population = world_cities_db_with_population[["NAME", "Country", "Population"]]
+    world_cities_db_with_population.dropna(inplace=True)
 
-    # Get population
-    cities_population = cities_db_with_population[["Population", "NAME"]]
-    cities_population.dropna(inplace=True)
-    # Find intersection
-    intersection = pd.merge(cities_db, cities_population, how='inner', on=['NAME'])
+    # Get world cities db with density of population
+    world_cities_db_with_density = pd.merge(world_cities_db_with_area, world_cities_db_with_population, how="inner", on=["NAME"])
+    world_cities_db_with_density["Density"] = world_cities_db_with_density["Population"] / world_cities_db_with_density["AREA"]
 
-    # Find density of population
-    intersection["DENSITY_OF_POPULATION"] = intersection["Population"] / intersection["AREA"]
+    world_cities_db_with_area = pd.merge(world_cities_db_with_area, world_cities_db_with_population, how="inner", on=["NAME"])
 
-    # Find main statistic parameters
-    population_statistics = basic_statistic_values(intersection["Population"])
-    print("Min population:", population_statistics['min'])
-    print("Max population:", population_statistics['max'])
-    print("Mean population:", population_statistics['mean'])
-    print("Median population:", population_statistics['median'])
-    print("Std population:", population_statistics['std'])
+    # Get basic statistics for world
+    world_population_statistics = basic_statistic_values(world_cities_db_with_population["Population"])
+    world_area_statistics = basic_statistic_values(world_cities_db_with_area["AREA"])
+    world_density_statistics = basic_statistic_values(world_cities_db_with_density["Density"])
+    results["World"] = {"population" : world_population_statistics, "area" : world_area_statistics, "density" : world_density_statistics}
 
-    area_statistics = basic_statistic_values(intersection["AREA"])
-    print("Min area:", area_statistics['min'])
-    print("Max area:", area_statistics['max'])
-    print("Mean area:", area_statistics['mean'])
-    print("Median area:", area_statistics['median'])
-    print("Std area:", area_statistics['std'])
+    # Create hists for world
+    create_plot(world_cities_db_with_population, "Population", "world", 30, "n cities", "Population")
+    create_plot(world_cities_db_with_area, "AREA", "world", 30, "n cities", r'$\mathregular{km^2}$')
+    create_plot(world_cities_db_with_density, "Density", "world", 30, "n cities", "Density")
 
-    density_of_population_statistics = basic_statistic_values(intersection["DENSITY_OF_POPULATION"])
-    print("Min density of population:", density_of_population_statistics['min'])
-    print("Max density of population:", density_of_population_statistics['max'])
-    print("Mean density of population:", density_of_population_statistics['mean'])
-    print("Median density of population:", density_of_population_statistics['median'])
-    print("Std density of population:", density_of_population_statistics['std'])
+    iso_codes = get_country_codes()
 
-    # Create plots
-    create_plot(intersection, "Population", "population", 30, "n cities", "Population")
-    create_plot(intersection, "AREA", "area", 30, "n cities", "Area")
-    create_plot(intersection, "DENSITY_OF_POPULATION", "density_of_population", 30, "n_cities", "Density of population")
+    # Make analysis for continents
+    for continent in continents:
+        # Get iso codes for specific continent
+        continent_iso_codes = pd.DataFrame()
+        continent_iso_codes["Country"] = iso_codes[continent].dropna().map(lambda x: x.lower())
+
+        # Create db with population in specific continent and apply analysis
+        continent_cities_with_population = pd.merge(world_cities_db_with_population, continent_iso_codes, how="inner", on=["Country"])
+        continent_population_statistics = basic_statistic_values(continent_cities_with_population["Population"])
+
+        # Create db with area in specific continent and apply analysis
+        continent_cities_with_area = pd.merge(world_cities_db_with_area, continent_iso_codes, how="inner", on=["Country"])
+        continent_area_statistics = basic_statistic_values(continent_cities_with_area["AREA"])
+
+        # Create db with dencity in specific continent and apply analysis
+        continent_cities_with_density = pd.merge(world_cities_db_with_density, continent_iso_codes, how="inner", on=["Country"])
+        continent_density_statistics = basic_statistic_values(continent_cities_with_density["Density"])
+
+        results[continent] = {"population" : continent_population_statistics, "area" : continent_area_statistics, "density" : continent_density_statistics}
+
+        # Create plot for continents
+        create_plot(continent_cities_with_population, "Population", continent, 30, "n cities", "Population")
+        create_plot(continent_cities_with_area, "AREA", continent, 30, "n cities", r'$\mathregular{km^2}$')
+        create_plot(continent_cities_with_density, "Density", continent, 30, "n cities", "Density")
+
+    # Make analysis for cities with population larger than 100 000
+    for number in population_ranges:
+        world_cities_db_with_population_filter = world_cities_db_with_population[world_cities_db_with_population["Population"] > number]
+        world_cities_db_with_population_filter_statistics = basic_statistic_values(world_cities_db_with_population_filter["Population"])
+
+        world_cities_db_with_area_filter = world_cities_db_with_density[world_cities_db_with_density["Population"] > number]
+        world_cities_db_with_area_filter_statistics = basic_statistic_values(world_cities_db_with_area_filter["AREA"])
+        world_cities_db_with_density_filter_statistics = basic_statistic_values(world_cities_db_with_area_filter["Density"])
+
+        results["larger_than_" + str(number)] = {"population" : world_cities_db_with_population_filter_statistics, "area" : world_cities_db_with_area_filter_statistics,
+                                         "density" : world_cities_db_with_density_filter_statistics}
+
+        # Create plot for continents
+        create_plot(world_cities_db_with_population_filter, "Population", "population_larger_" + str(number), 30, "n cities", "Population")
+        create_plot(world_cities_db_with_area_filter, "AREA", "population_larger_" + str(number), 30, "n cities", r'$\mathregular{km^2}$')
+        create_plot(world_cities_db_with_area_filter, "Density", "population_larger_" + str(number), 30, "n cities", "Density")
+
+
+    results_df = pd.DataFrame(results)
+
+    # results_df.to_json("results.json")
+
+
+
+
+
+
+
+
+
 
 
 
